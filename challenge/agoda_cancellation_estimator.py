2df1a06f0b74121c041e53e76bebc466dea20e63
Niv <niv.eckhaus@gmail.com>
2022-04-24 19:10:39 +0300

    our group submission for week 3

--- challenge/agoda_cancellation_estimator.py
+++ challenge/agoda_cancellation_estimator.py
@@ -2,6 +2,14 @@ from __future__ import annotations
 from typing import NoReturn
 from IMLearn.base import BaseEstimator
 import numpy as np
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis,\
+    QuadraticDiscriminantAnalysis
 
 
 class AgodaCancellationEstimator(BaseEstimator):
@@ -22,6 +30,25 @@ class AgodaCancellationEstimator(BaseEstimator):
 
         """
         super().__init__()
+        # # Original:
+        # Over week1 and week2, LinearRegression is best with threshold = 0.08
+        self.model = LinearRegression()
+        # # New tries:
+        # self.model = LogisticRegression() # 10
+        # self.model = DecisionTreeClassifier(max_depth=2) # 11
+        # self.model = DecisionTreeClassifier(max_depth=5) # 12
+        # self.model = KNeighborsClassifier(n_neighbors=5) # 13
+        # self.model = KNeighborsClassifier(n_neighbors=10) # 14
+        # self.model = make_pipeline(PolynomialFeatures(2),
+        #                            LinearRegression(fit_intercept=False)) #15
+        # Over train-test split, polynomial is best (k=3 with threshold=0.25)
+        # self.model = make_pipeline(PolynomialFeatures(3),
+        #                            LinearRegression(fit_intercept=False)) #16
+        # self.model = make_pipeline(PolynomialFeatures(5),
+        #                            LinearRegression(fit_intercept=False)) #17
+        # self.model = SVC(gamma="auto") #18
+        # self.model = LinearDiscriminantAnalysis(store_covariance=True) #19
+        # self.model = QuadraticDiscriminantAnalysis(store_covariance=True) #20
 
     def _fit(self, X: np.ndarray, y: np.ndarray) -> NoReturn:
         """
@@ -39,9 +66,16 @@ class AgodaCancellationEstimator(BaseEstimator):
         -----
 
         """
-        pass
+        self.model.fit(X, y)
 
     def _predict(self, X: np.ndarray) -> np.ndarray:
+        """
+        this is a fictive function, only for the API to run
+        """
+        return X
+
+    def predict_with_threshold(self, X: np.ndarray, threshold: float = 0.5) \
+            -> np.ndarray:
         """
         Predict responses for given samples using fitted estimator
 
@@ -55,7 +89,8 @@ class AgodaCancellationEstimator(BaseEstimator):
         responses : ndarray of shape (n_samples, )
             Predicted responses of given samples
         """
-        return np.zeros(X.shape[0])
+        return np.array([1 if i >= threshold else 0
+                         for i in self.model.predict(X)])
 
     def _loss(self, X: np.ndarray, y: np.ndarray) -> float:
         """
@@ -74,4 +109,34 @@ class AgodaCancellationEstimator(BaseEstimator):
         loss : float
             Performance under loss function
         """
-        pass
+        f1_macros = []
+        threshold_options = [0.01, 0.1, 0.25, 0.5, 0.75, 0.9]
+        # threshold_options = [i / 100 for i in range(1, 11)]
+        for threshold in threshold_options:
+            res = self.predict_with_threshold(X, threshold)
+            tp1 = tn1 = fp1 = fn1 = 0
+            tp0 = tn0 = fp0 = fn0 = 0
+            for index, i in enumerate(res):
+                if y[index] == i:
+                    if i == 1:
+                        tp1 += 1
+                        tn0 += 1
+                    else:
+                        tn1 += 1
+                        tp0 += 1
+                else:
+                    if i == 1:
+                        fp1 += 1
+                        fn0 += 1
+                    else:
+                        fn1 += 1
+                        fp0 += 1
+            f1_1 = tp1 / (tp1 + 0.5 * (fp1 + fn1))
+            f1_0 = tp0 / (tp0 + 0.5 * (fp0 + fn0))
+            f1_macro = (f1_0 + f1_1) * 0.5
+            f1_macros.append(f1_macro)
+            accuracy = (tp1 + tn1) / len(res)
+            print(f"threshold: {threshold}, f1 for 1s: {f1_1},"
+                  f" f1 for 0s: {f1_0}, , f1 macro: {f1_macro}, "
+                  f"accuracy: {accuracy}")
+        return max(f1_macros)
--- challenge/agoda_cancellation_prediction.py
+++ challenge/agoda_cancellation_prediction.py
@@ -1,17 +1,25 @@
 from challenge.agoda_cancellation_estimator import AgodaCancellationEstimator
 from IMLearn.utils import split_train_test
+from typing import Tuple
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline
 
 import numpy as np
 import pandas as pd
+import datetime
 
 
-def load_data(filename: str):
+def load_data(filename: str, is_train: bool = False):
     """
     Load Agoda booking cancellation dataset
     Parameters
     ----------
     filename: str
         Path to house prices dataset
+    is_train: bool
+        Whether it is training set
 
     Returns
     -------
@@ -20,19 +28,195 @@ def load_data(filename: str):
     2) Tuple of pandas.DataFrame and Series
     3) Tuple of ndarray of shape (n_samples, n_features) and ndarray of shape (n_samples,)
     """
-    # TODO - replace below code with any desired preprocessing
-    full_data = pd.read_csv(filename).dropna().drop_duplicates()
-    features = full_data[["h_booking_id",
-                          "hotel_id",
-                          "accommadation_type_name",
-                          "hotel_star_rating",
-                          "customer_nationality"]]
-    labels = full_data["cancellation_datetime"]
+    # Trying to use only interesting columns;
 
-    return features, labels
+    # Originally used only:
+    # 'original_selling_amount', 'is_user_logged_in', 'has_special_request',
+    #    'days_in_advance', 'duration', 'guest_from_country',
+    #    'percentage_of_payment_if_cancel_in_relevant_week',
+    #    'amount_of_payment_if_cancel_in_relevant_week',
+    #    'is_it_last_week_before_increase', 'has_no_show_policy'
+
+    # Later tried using more: (but it didn't improve)
+    # 'hotel_star_rating', 'guest_is_not_the_customer', 'no_of_adults',
+    #    'no_of_children', 'no_of_extra_bed', 'no_of_room',
+    #    'original_selling_amount', 'is_user_logged_in', 'request_nonesmoke',
+    #    'request_latecheckin', 'request_highfloor', 'request_largebed',
+    #    'request_twinbeds', 'request_airport', 'request_earlycheckin',
+    #    'has_special_request', 'days_in_advance', 'duration',
+    #    'guest_from_country',
+    #    'percentage_of_payment_if_cancel_in_relevant_week',
+    #    'amount_of_payment_if_cancel_in_relevant_week',
+    #    'is_it_last_week_before_increase', 'has_no_show_policy'
+
+    df = pd.read_csv(filename)
+    date_columns = ["booking_datetime", "checkin_date", "checkout_date",
+                    "cancellation_datetime", "hotel_live_date"]
+    if not is_train:
+        date_columns.remove("cancellation_datetime")
+    for column in date_columns:
+        df[column] = pd.to_datetime(df[column]).dt.floor("D")
+
+    if is_train:
+        # save only data where checkin is strictly after 15 of the next month,
+        # in order to mimic test set
+        df = df[df["booking_datetime"]
+                + pd.to_timedelta((df["booking_datetime"].dt.days_in_month
+                                   - df["booking_datetime"].dt.day + 15),
+                                  unit="D")
+                <= df["checkin_date"]]
+
+    df["start_of_week_next_month"] = df["booking_datetime"] + pd.to_timedelta(
+        (df["booking_datetime"].dt.days_in_month
+         - df["booking_datetime"].dt.day + 7), unit="D")
+
+    df["is_user_logged_in"] = df["is_user_logged_in"].astype(int)
+
+    columns_to_drop = ["cancellation_datetime",
+                       "h_booking_id", "booking_datetime",
+                       "checkin_date", "checkout_date",
+                       "hotel_id", "hotel_country_code", "hotel_live_date",
+                       "accommadation_type_name",
+                       "charge_option", "h_customer_id",
+                       "customer_nationality",
+                       "guest_nationality_country_name",
+                       "origin_country_code",
+                       "language", "original_payment_method",
+                       "original_payment_type", "original_payment_currency",
+                       "cancellation_policy_code", "is_first_booking",
+                       "hotel_area_code", "hotel_brand_code",
+                       "hotel_city_code", "hotel_chain_code",
+                       "start_of_week_next_month",
+                       # Columns I tried to return:
+                       "hotel_star_rating", "no_of_room",
+                       "guest_is_not_the_customer",
+                       "no_of_adults", "no_of_children", "no_of_extra_bed",
+                       "cancelled_in_these_dates"]
+
+    df["has_special_request"] = 0
+    special_requests_prefix = "request_"
+    for column in df.columns:
+        if column.startswith(special_requests_prefix):
+            df[column] = df[column].fillna(0)
+            df["has_special_request"] = df["has_special_request"] + df[column]
+            # Columns I tried to return:
+            columns_to_drop.append(column)
+    df["has_special_request"] = df["has_special_request"].apply(
+        lambda x: int(x > 0))
+
+    df["days_in_advance"] = (df["checkin_date"]
+                             - df["booking_datetime"]).dt.days
+    df["duration"] = (df["checkout_date"] - df["checkin_date"]).dt.days
+
+    df["guest_from_country"] = df["hotel_country_code"] == df[
+                                                        "origin_country_code"]
+    df["guest_from_country"] = df["guest_from_country"].astype(int)
+
+    # Columns I tried to return:
+    # charge_option_dict = {"Pay Now": 0, "Pay Later": 1, "Pay at Check-in": 2}
+    # df["charge_option"] = df["charge_option"].apply(lambda x:
+    #                                                 charge_option_dict.get(x,
+    #                                                                       0))
+
+    if is_train:
+        df = df[df.cancellation_policy_code != "UNKNOWN"]
+
+    df["percentage_of_payment_if_cancel_in_relevant_week"] = 0
+    df["amount_of_payment_if_cancel_in_relevant_week"] = 0
+    df["is_it_last_week_before_increase"] = 0
+    has_noshow_policy = []
+    cancelled_in_these_dates = []
+    for i, row in df.iterrows():
+        days_to_cancel, has_noshow, all_policies = _parse_cancellation_code(
+            row['cancellation_policy_code'])
+        descending_keys = list(all_policies)
+        descending_keys.sort(reverse=True)
+        relevant_policy_days = None
+        if all_policies:
+            for num_of_days in descending_keys:
+                relevant_day_until_checkin = df.loc[i, "checkin_date"] \
+                        - pd.to_timedelta(num_of_days, unit="D")
+                if relevant_day_until_checkin \
+                        >= df.loc[i, "start_of_week_next_month"]:
+                    relevant_policy_days = num_of_days
+                if df.loc[i, "start_of_week_next_month"] \
+                        + pd.to_timedelta(8, unit="D") \
+                        >= relevant_day_until_checkin \
+                        > df.loc[i, "start_of_week_next_month"]:
+                    df.loc[i, "is_it_last_week_before_increase"] = 1
+        if relevant_policy_days:
+            if all_policies[relevant_policy_days][1] == "P":
+                df.loc[i, "percentage_of_payment_if_cancel_in_relevant_week"] \
+                    = all_policies[relevant_policy_days][0] / 100
+                df.loc[i, "amount_of_payment_if_cancel_in_relevant_week"] \
+                    = all_policies[relevant_policy_days][0] / 100 * \
+                      df.loc[i, "original_selling_amount"]
+            else: # it is "N"
+                df.loc[i, "percentage_of_payment_if_cancel_in_relevant_week"] \
+                    = all_policies[relevant_policy_days][0] / \
+                      df.loc[i, "duration"]
+                df.loc[i, "amount_of_payment_if_cancel_in_relevant_week"] \
+                    = all_policies[relevant_policy_days][0] / \
+                      df.loc[i, "duration"] * \
+                      df.loc[i, "original_selling_amount"]
+
+        has_noshow_policy.append(int(has_noshow))
+        if is_train:
+            cancellation_time = row["cancellation_datetime"]
+            if not isinstance(cancellation_time, datetime.date) or pd.isna(
+                    cancellation_time):
+                cancelled_in_these_dates.append(0)
+            else:
+                was_canceled = row["start_of_week_next_month"] <= \
+                               row["cancellation_datetime"] < \
+                               row["start_of_week_next_month"] + \
+                               pd.to_timedelta(7, unit="D")
+                cancelled_in_these_dates.append(int(was_canceled))
+    df["has_no_show_policy"] = has_noshow_policy
+    if is_train:
+        df["cancelled_in_these_dates"] = cancelled_in_these_dates
+    else:
+        df['cancelled_in_these_dates'] = 0
+
+    results = df["cancelled_in_these_dates"]
+
+    if not is_train:
+        columns_to_drop.remove("cancellation_datetime")
+    df.drop(columns_to_drop, axis=1, inplace=True)
+
+    return df, results
 
 
-def evaluate_and_export(estimator: BaseEstimator, X: np.ndarray, filename: str):
+def _parse_cancellation_code(code: str) -> Tuple[int, bool, dict]:
+    """
+    parses cancellation code to days to cancel and whether there is a
+    no show policy.
+    addition: a dict of all cancellation codes
+    """
+    if not code or not isinstance(code, str):
+        return 0, False, dict()
+    if code == "UNKNOWN":
+        # # Original:
+        # return 5, True
+        # # My change:
+        return 0, True, dict()
+    parts = code.split("_")
+    has_no_show_policy = False
+    days_in_advance = 0
+    policy_by_days = dict()
+    for part in parts:
+        if "D" not in part:
+            has_no_show_policy = True
+            continue
+        else:
+            num_of_days = int(part[:part.find("D")])
+            days_in_advance = max(days_in_advance, num_of_days)
+            policy_by_days[num_of_days] = (int(part[part.find("D") + 1:-1]),
+                                           part[-1])
+    return days_in_advance, has_no_show_policy, policy_by_days
+
+
+def evaluate_and_export(estimator, X: np.ndarray, filename: str):
     """
     Export to specified file the prediction results of given estimator on given testset.
 
@@ -51,18 +235,43 @@ def evaluate_and_export(estimator: BaseEstimator, X: np.ndarray, filename: str):
         path to store file at
 
     """
-    pd.DataFrame(estimator.predict(X), columns=["predicted_values"]).to_csv(filename, index=False)
+    pd.DataFrame(estimator.predict_with_threshold(X, threshold=0.08),
+                 columns=["predicted_values"]).to_csv(filename, index=False)
 
 
 if __name__ == '__main__':
     np.random.seed(0)
 
     # Load data
-    df, cancellation_labels = load_data("../datasets/agoda_cancellation_train.csv")
-    train_X, train_y, test_X, test_y = split_train_test(df, cancellation_labels)
+    X, y = load_data("../datasets/agoda_cancellation_train.csv", is_train=True)
+
+    test, _ = load_data("./test_set_week_3.csv", is_train=False)
+
+    # r"C:\Users\nivec\HUJI Drive\Year 2\Semester B\IML\IML.HUJI\challenge\test_set_week_1_labels.csv"
+    # week1_X, _ = load_data("./test_set_week_1.csv", is_train=False)
+    # week1_y = pd.read_csv("./test_set_week_1_labels.csv")["label"]
+    # week2_X, _ = load_data("./test_set_week_2.csv", is_train=False)
+    # week2_y = pd.read_csv("./test_set_week_2_labels.csv")["label"]
+    #
+    # train_X, test_X, train_y, test_y = train_test_split(X, y, test_size=0.25)
+
+    # print("Week 1 loss test")
+    # check_estimator_on_labels1 = AgodaCancellationEstimator()
+    # check_estimator_on_labels1.fit(X, y)
+    # check_estimator_on_labels1.loss(week1_X, week1_y)
+    # print("Week 2 loss test")
+    # check_estimator_on_labels2 = AgodaCancellationEstimator()
+    # check_estimator_on_labels2.fit(X, y)
+    # check_estimator_on_labels2.loss(week2_X, week2_y)
+    # print("Train-Test partition loss test")
+    # check_estimator_on_train = AgodaCancellationEstimator()
+    # check_estimator_on_train.fit(train_X, train_y)
+    # check_estimator_on_train.loss(test_X.to_numpy(), test_y.to_numpy())
 
     # Fit model over data
-    estimator = AgodaCancellationEstimator().fit(train_X, train_y)
+    estimator = AgodaCancellationEstimator()
+    estimator.fit(X, y)
 
     # Store model predictions over test set
-    evaluate_and_export(estimator, test_X, "id1_id2_id3.csv")
+    evaluate_and_export(estimator, test.to_numpy(),
+                        "2055501016_208543116_207129420.csv")
